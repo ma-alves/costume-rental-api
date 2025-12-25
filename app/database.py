3561d6db13f85517c202a1aca1f8e88d1ccc3286
matheus <matheusamenos@gmail.com>
2025-12-25 17:44:46 -0300

    Resolve async context manager bugado em database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,26 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import AsyncGenerator
 
 from .settings import Settings
 
 
-engine = create_async_engine(Settings().DATABASE_URL)
-# Base.metadata.create_all(bind=engine)
+DATABASE_URL = Settings().DATABASE_URL
 
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(
+    async_engine, 
+    expire_on_commit=False, 
+    class_=AsyncSession,
+    autoflush=False
+)
 
-async def get_session():
-	with AsyncSession(engine, expire_on_commit=False) as session:
-		yield session
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit() # Commit changes if the request is successful
+        except Exception:
+            await session.rollback() # Rollback on exception
+            raise
+        finally:
+            await session.close() # Ensure the session is closed
